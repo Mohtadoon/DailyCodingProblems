532e6c6d6bd418bd27dcaa1fd205ce9d02b53ce1
Mohtadoon Shaik <shaik.mohtadoon@gmail.com>
2022-03-17 09:17:10 -0500

    refactoored and added missing number in array

---
+++ src/dcp/algorithms/DuplicatesCheckWithoutMap.java
@@ -0,0 +1,27 @@
+package dcp.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DuplicatesCheckWithoutMap {
+
+
+    public static void main(String args[])
+    {
+        //int numRay[] = { 0, 4, 3, 2, 7, 8, 2, 3, 1 };
+        int numRay[] = { 0, 1, 1};
+        int n = numRay.length;
+
+        for(int i=0; i < numRay.length; i++){
+            numRay[numRay[i] % n] = numRay[numRay[i] % n] + n;
+        }
+
+        for(int i=0; i < numRay.length; i++){
+            if(numRay[i] / n > 1){
+                System.out.println(i + " ");
+            }
+        }
+
+    }
+
+}
